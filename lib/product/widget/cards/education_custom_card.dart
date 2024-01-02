97eeb396f2da88bee486297ca71b7487c7b4ba70
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-02 12:18:14 +0300

    education card design

--- lib/product/widget/cards/education_custom_card.dart
+++ lib/product/widget/cards/education_custom_card.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/feature/education/word_study_page.dart';
 import 'package:flutter_application/product/constants/index.dart';
 import 'package:kartal/kartal.dart';
 
+import '../../Vocabulary/color_datas.dart';
+
 class EducationCustomCard extends StatefulWidget {
   const EducationCustomCard(
-      {super.key, required this.name, required this.count, required this.img});
+      {super.key, required this.name, required this.img, required this.words});
 
   final String name;
-  final int count;
+
   final Image img;
+  final List<Word> words;
 
   @override
   State<EducationCustomCard> createState() => _MyWidgetState();
@@ -19,31 +23,43 @@ class _MyWidgetState extends State<EducationCustomCard> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 400,
-      child: Card(
-        elevation: 3,
-        color: ColorConstants.actOfWrath,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            children: [
-              SizedBox(height: 150, width: 170, child: widget.img),
-              const SizedBox(
-                width: 50,
+      child: InkWell(
+        onTap: () {},
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => WordStudy(words: widget.words)));
+          },
+          child: Card(
+            elevation: 3,
+            color: ColorConstants.actOfWrath,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  SizedBox(height: 150, width: 170, child: widget.img),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: [
+                    Text(
+                      widget.name,
+                      style: context.general.textTheme.headlineSmall!
+                          .copyWith(color: ColorConstants.white),
+                    ),
+                    Text(
+                      '${widget.words.length}  Word',
+                      style: context.general.textTheme.bodySmall
+                          ?.copyWith(color: Colors.white),
+                    )
+                  ])
+                ],
               ),
-              Column(children: [
-                Text(
-                  widget.name,
-                  style: context.general.textTheme.headlineSmall!
-                      .copyWith(color: ColorConstants.white),
-                ),
-                Text(
-                  '${widget.count}  Word',
-                  style: context.general.textTheme.bodySmall
-                      ?.copyWith(color: Colors.white),
-                )
-              ])
-            ],
+            ),
           ),
         ),
       ),
