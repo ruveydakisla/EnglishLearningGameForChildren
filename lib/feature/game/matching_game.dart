26206c2cf2d09b5a579ea39520d6a3dda2c7acf9
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-05 12:35:53 +0300

    matching game fixed

--- lib/feature/game/matching_game.dart
+++ lib/feature/game/matching_game.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application/feature/game/start_page.dart';
+import 'package:flutter_application/core/services/cloud_services.dart';
 import 'package:flutter_application/product/Vocabulary/color_datas.dart';
+import 'package:flutter_application/product/constants/color_constants.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kartal/kartal.dart';
 import 'components/custom_card.dart';
 
 class MatchingGame extends StatefulWidget {
@@ -18,6 +22,7 @@ class _GamePageState extends State<MatchingGame> {
   List<bool> isVisibleList = [];
   int matchedCount = 0;
   int elapsedTime = 0;
+  int totalScore = 0;
   bool gameEnded = false;
   late Timer timer;
 
@@ -61,23 +66,31 @@ class _GamePageState extends State<MatchingGame> {
   void endGame() {
     gameEnded = true;
     timer.cancel();
+
+    CloudServices()
+        .addScoreToUser(FirebaseAuth.instance.currentUser!.email!, totalScore);
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: const Text('Oyun Bitti'),
         content: Text(
-            'Toplam süre: $elapsedTime saniye\nEşleştirilen: $matchedCount'),
+            'Toplam süre: $elapsedTime saniye\nEşleştirilen: $matchedCount\nPuanınız: $totalScore'),
         actions: [
           TextButton(
             onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const StartPage(),
-                ),
-              );
+              Navigator.pop(context);
+              resetGame();
             },
             child: const Text('Tamam'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              resetGame();
+              isVisibleList = List.generate(words.length * 2, (index) => true);
+              startGame();
+            },
+            child: const Text('Start Again'),
           ),
         ],
       ),
@@ -125,11 +138,15 @@ class _GamePageState extends State<MatchingGame> {
       int firstIndex = tappedIndices[0];
       int secondIndex = tappedIndices[1];
 
-      if (words[firstIndex].url == words[secondIndex].url) {
+      if (words[firstIndex].name == words[secondIndex].name) {
         setState(() {
-          matchedCount++;
           isVisibleList[firstIndex] = false;
           isVisibleList[secondIndex] = false;
+          totalScore += 10;
+          matchedCount++; // Her eşleşme için 10 puan
+          if (matchedCount == words.length / 2) {
+            endGame(); // Eğer bütün eşleştirmeler tamamlandıysa oyunu bitir
+          }
         });
       } else {
         Future.delayed(const Duration(milliseconds: 500), () {
@@ -138,10 +155,6 @@ class _GamePageState extends State<MatchingGame> {
             isTappedList[secondIndex] = false;
           });
         });
-      }
-
-      if (matchedCount == words.length) {
-        endGame();
       }
 
       setState(() {
@@ -153,46 +166,85 @@ class _GamePageState extends State<MatchingGame> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Eşleştirme Oyunu'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: ColorConstants.actOfWrath,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20))),
+                    margin: EdgeInsets.zero,
+                    width: 300,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Time',
+                            style: context.general.textTheme.bodyLarge
+                                ?.copyWith(color: ColorConstants.white),
+                          ),
+                          Text(
+                            elapsedTime.toString(),
+                            style: GoogleFonts.bungeeSpice()
+                                .copyWith(fontSize: 30),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.zero,
+                    width: 500,
+                    color: ColorConstants.darkKnight,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Matched ',
+                          style: context.general.textTheme.bodyLarge?.copyWith(
+                              fontSize: 20, color: ColorConstants.white),
+                        ),
+                        Text(
+                          matchedCount.toString(),
+                          style:
+                              GoogleFonts.bungeeSpice().copyWith(fontSize: 30),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: words.length,
-              itemBuilder: (context, index) {
-                return CustomCard(
-                  word: words[index],
-                  isVisible:
-                      isVisibleList.isNotEmpty && index < isVisibleList.length
-                          ? isVisibleList[index]
-                          : false,
-                  onTap: () {
-                    onCardTapped(index);
-                  },
-                );
-              },
-            ),
-            const SizedBox(height: 20),
-            Text('Eşleştirilen Sayısı: $matchedCount'),
-            Text('Geçen Süre: $elapsedTime saniye'),
-            ElevatedButton(
-              onPressed: () {
-                endGame();
-              },
-              child: const Text('Oyunu Bitir'),
-            ),
-          ],
+              const SizedBox(height: 20),
+              GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                ),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: words.length,
+                itemBuilder: (context, index) {
+                  return CustomCard(
+                    word: words[index],
+                    isVisible:
+                        isVisibleList.isNotEmpty && index < isVisibleList.length
+                            ? isVisibleList[index]
+                            : false,
+                    onTap: () {
+                      onCardTapped(index);
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
