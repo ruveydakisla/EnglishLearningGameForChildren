0239eb1f514046eb8aa9a5c49fd1830f3a7f9c25
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-06 15:40:52 +0300

    word list changed

--- lib/feature/education/word_list.dart
+++ lib/feature/education/word_list.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/product/Vocabulary/body_section_datas.dart';
+import 'package:flutter_application/product/Vocabulary/clothes_datas.dart';
+import 'package:flutter_application/product/Vocabulary/fruits_datas.dart';
+import 'package:flutter_application/product/Vocabulary/numbers_datas.dart';
+import 'package:flutter_application/product/Vocabulary/school_stuff_datas.dart';
+import 'package:flutter_application/product/Vocabulary/shapes_datas.dart';
+import 'package:flutter_application/product/Vocabulary/vegetables.dart';
+import 'package:flutter_application/product/Vocabulary/vehicle_datas.dart';
+import 'package:flutter_application/product/Vocabulary/weather_datas.dart';
 import 'package:flutter_application/product/constants/image_constants.dart';
 
 import '../../product/Vocabulary/animals_datas.dart';
@@ -19,6 +28,42 @@ class StudyTopics {
     StudyTopics(
         name: 'Colors',
         img: ImageConstants.colorsImg.toImg,
-        wordList: Colorr.colors)
+        wordList: Colorr.colors),
+    StudyTopics(
+        name: 'Numbers',
+        img: ImageConstants.numberImg.toImg,
+        wordList: Numbers.numbers),
+    StudyTopics(
+        name: 'Fruits',
+        img: ImageConstants.fruitsImg.toImg,
+        wordList: Fruits.fruits),
+    StudyTopics(
+        name: 'Vegetables',
+        img: ImageConstants.vegetablesImg.toImg,
+        wordList: Vegetables.vegetables),
+    StudyTopics(
+        name: 'Weather',
+        img: ImageConstants.weathersImg.toImg,
+        wordList: Weather.weathers),
+    StudyTopics(
+        name: 'Vehicles',
+        img: ImageConstants.vehiclesImg.toImg,
+        wordList: Vehicle.vehicles),
+    StudyTopics(
+        name: 'Body Section',
+        img: ImageConstants.bodySectionsImg.toImg,
+        wordList: BodySection.sections),
+    StudyTopics(
+        name: 'Clothes',
+        img: ImageConstants.clothesImg.toImg,
+        wordList: Clothes.clothes),
+    StudyTopics(
+        name: 'School Stuff',
+        img: ImageConstants.schoolStuffImg.toImg,
+        wordList: SchoolStuff.stuffs),
+    StudyTopics(
+        name: 'Shapes',
+        img: ImageConstants.shapesImg.toImg,
+        wordList: Shapes.shapes),
   ];
 }
