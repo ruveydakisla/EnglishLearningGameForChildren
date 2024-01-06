b3bf05c32ab71aee56ad7f6e984c1fa76a970a87
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-06 15:43:11 +0300

    new img

--- lib/feature/signIn/signIn.dart
+++ lib/feature/signIn/signIn.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/core/services/auth_services.dart';
+import 'package:flutter_application/core/services/cloud_services.dart';
 import 'package:flutter_application/feature/navbar/top_navbar.dart';
+import 'package:flutter_application/feature/signup/signup_view.dart';
+import 'package:flutter_application/product/constants/icons_constants.dart';
 import 'package:flutter_application/product/constants/index.dart';
 import 'package:flutter_application/product/widget/buttons/custom_elevated_button.dart';
 import 'package:flutter_application/product/widget/textfields/text_field.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kartal/kartal.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -16,19 +22,20 @@ class _SignInPageState extends _SignInPageAbstract {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffFFF5C2),
+      appBar: AppBar(
+        title: Text(
+          StringConstants.appName,
+          style: GoogleFonts.sevillana(),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: ColorConstants.white,
       body: ListView(children: [
         Container(
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              const SizedBox(
-                height: 80,
-              ),
               ImageConstants.signInImg.toImg,
-              const SizedBox(
-                height: 40,
-              ),
               CustomTextField(
                 labelText: StringConstants.email,
                 controller: emailController,
@@ -50,6 +57,48 @@ class _SignInPageState extends _SignInPageAbstract {
                 buttonText: StringConstants.signIn,
                 onPressed: signInUser,
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 50,
+                width: 300,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          side: const BorderSide(
+                              color: ColorConstants.ottomanRed, width: 3))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Sign In with Google',
+                        style: context.general.textTheme.bodyMedium!
+                            .copyWith(color: Colors.black),
+                      ),
+                      SizedBox(width: 40, child: IconConstants.googleIcon.toImg)
+                    ],
+                  ),
+                  onPressed: () {
+                    CloudServices().signUpWithGoogle();
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Don\'t you have account?'),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignupView()));
+                      },
+                      child: const Text('Sign Up'))
+                ],
+              )
             ],
           ),
         ),
