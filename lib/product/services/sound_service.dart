1e264cf029c282c05c709d537570c2f2e7112c66
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-06 15:43:47 +0300

    sound service changed

--- lib/product/services/sound_service.dart
+++ lib/product/services/sound_service.dart
@@ -1,11 +1,17 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:text_to_speech/text_to_speech.dart';
 
 class SoundService {
-  final player = AudioPlayer();
+  // final player = AudioPlayer();
 
-  play(String word) async {
-    await player.play(UrlSource(
-        'https://translate.google.com/translate_tts?ie=UTF-8&q=$word&tl=en&client=tw-ob'));
+  // play(String word) async {
+  //   await player.play(UrlSource(
+  //       'https://translate.google.com/translate_tts?ie=UTF-8&q=$word&tl=en&client=tw-ob'));
+  // }
+
+  TextToSpeech tts = TextToSpeech();
+  play(String word) {
+    tts.speak(word);
   }
 }
