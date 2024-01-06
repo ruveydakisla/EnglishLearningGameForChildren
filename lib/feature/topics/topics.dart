f071d3cdecf55af336cb3d7f4a0880d1fac2f70e
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-06 15:42:24 +0300

    new topics added

--- lib/feature/topics/topics.dart
+++ lib/feature/topics/topics.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/product/Vocabulary/animals_datas.dart';
+import 'package:flutter_application/product/Vocabulary/body_section_datas.dart';
+import 'package:flutter_application/product/Vocabulary/clothes_datas.dart';
 import 'package:flutter_application/product/Vocabulary/color_datas.dart';
+import 'package:flutter_application/product/Vocabulary/fruits_datas.dart';
+import 'package:flutter_application/product/Vocabulary/numbers_datas.dart';
+import 'package:flutter_application/product/Vocabulary/school_stuff_datas.dart';
+import 'package:flutter_application/product/Vocabulary/shapes_datas.dart';
+import 'package:flutter_application/product/Vocabulary/vegetables.dart';
+import 'package:flutter_application/product/Vocabulary/vehicle_datas.dart';
+import 'package:flutter_application/product/Vocabulary/weather_datas.dart';
 import 'package:flutter_application/product/constants/icons_constants.dart';
 import 'package:flutter_application/product/constants/index.dart';
 
@@ -22,16 +31,41 @@ class Topic {
         icon: IconConstants.colorsIcon.toImg,
         wordList: Colorr.colors),
     Topic(
-      name: 'Numbers',
-      icon: IconConstants.numbersIcon.toImg,
-    ),
-    Topic(name: 'Fruits', icon: IconConstants.fruitsIcon.toImg),
-    Topic(name: 'Vegetables', icon: IconConstants.vegetablesIcon.toImg),
-    Topic(name: 'Weather', icon: IconConstants.weatherIcon.toImg),
-    Topic(name: 'Vehicles', icon: IconConstants.vehiclesIcon.toImg),
-    Topic(name: 'Body Sections', icon: IconConstants.bodySectionItem.toImg),
-    Topic(name: 'Clothes', icon: IconConstants.clothesIcon.toImg),
-    Topic(name: 'School Stuff', icon: IconConstants.schoolStuffIcon.toImg),
-    Topic(name: 'Shapes', icon: IconConstants.shapesIcon.toImg)
+        name: 'Numbers',
+        icon: IconConstants.numbersIcon.toImg,
+        wordList: Numbers.numbers),
+    Topic(
+        name: 'Fruits',
+        icon: IconConstants.fruitsIcon.toImg,
+        wordList: Fruits.fruits),
+    Topic(
+        name: 'Vegetables',
+        icon: IconConstants.vegetablesIcon.toImg,
+        wordList: Vegetables.vegetables),
+    Topic(
+        name: 'Weather',
+        icon: IconConstants.weatherIcon.toImg,
+        wordList: Weather.weathers),
+    Topic(
+        name: 'Vehicles',
+        icon: IconConstants.vehiclesIcon.toImg,
+        wordList: Vehicle.vehicles),
+    Topic(
+        name: 'Body Sections',
+        icon: IconConstants.bodySectionItem.toImg,
+        wordList: BodySection.sections),
+    Topic(
+        name: 'Clothes',
+        icon: IconConstants.clothesIcon.toImg,
+        wordList: Clothes.clothes),
+    Topic(
+        name: 'School Stuff',
+        icon: IconConstants.schoolStuffIcon.toImg,
+        wordList: SchoolStuff.stuffs),
+    Topic(
+      name: 'Shapes',
+      icon: IconConstants.shapesIcon.toImg,
+      wordList: Shapes.shapes,
+    )
   ];
 }
