6b1ae124a1b51d82320d801939995f424c439954
ruveydakisla <ruveydakisla34@gmail.com>
2024-01-14 00:31:18 +0300

    new functions

--- lib/product/services/sound_service.dart
+++ lib/product/services/sound_service.dart
@@ -1,10 +1,34 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundService {
   final player = AudioPlayer();
+  final assetsAudioPlayer = AssetsAudioPlayer();
 
   play(String word) async {
     await player.play(UrlSource(
         'https://translate.google.com/translate_tts?ie=UTF-8&q=$word&tl=en&client=tw-ob'));
+  }
+
+  void playCorrect() {
+    assetsAudioPlayer.open(
+      Audio("assets/sounds/correctt.mp3"),
+    );
+  }
+
+  void playWrong() {
+    assetsAudioPlayer.open(
+      Audio("assets/sounds/soft.mp3"),
+    );
+  }
+
+  void playSong() {
+    assetsAudioPlayer.open(
+      Audio("assets/sounds/music.mp3"),
+    );
+  }
+
+  void stopSong() {
+    assetsAudioPlayer.stop();
   }
 }
